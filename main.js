6012a5e559ecbe53cc7d521608214d70853681c5
mhdhanji <dhanjimohammed@gmail.com>
2025-02-17 10:55:56 +0000

    Add system tray and background notification support

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray, Menu } = require('electron');
 const path = require('path');
 
+let mainWindow = null;
+let tray = null;
+
 function createWindow() {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: 1200,
         height: 800,
         webPreferences: {
@@ -13,12 +16,49 @@ function createWindow() {
         }
     });
 
+    // Prevent window from being garbage collected
+    mainWindow.on('close', function (event) {
+        if (!app.isQuitting) {
+            event.preventDefault();
+            mainWindow.hide();
+        }
+        return false;
+    });
+
+    // Load the index.html file
     mainWindow.loadFile('index.html');
-    // Uncomment for debugging
-    // mainWindow.webContents.openDevTools();
 }
 
-app.whenReady().then(createWindow);
+function createTray() {
+    tray = new Tray(path.join(__dirname, 'icon.ico')); // Make sure you have an icon.ico file
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Show App',
+            click: function () {
+                mainWindow.show();
+            }
+        },
+        {
+            label: 'Quit',
+            click: function () {
+                app.isQuitting = true;
+                app.quit();
+            }
+        }
+    ]);
+
+    tray.setToolTip('Van Timetable App');
+    tray.setContextMenu(contextMenu);
+
+    tray.on('click', () => {
+        mainWindow.show();
+    });
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    createTray();
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
