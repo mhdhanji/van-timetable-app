4324e74d66013731bc76f9c6c90294f3ed1f642b
mhdhanji <dhanjimohammed@gmail.com>
2025-03-03 15:16:41 +0000

    firebase

--- firebase-config.js
+++ firebase-config.js
@@ -2,21 +2,22 @@ const { initializeApp } = require('firebase/app');
 const { getFirestore } = require('firebase/firestore');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBkkSet1OXBhkJhdcdphWG_3yFUJFWrujI",
-    authDomain: "map-van-times.firebaseapp.com",
-    projectId: "map-van-times",
-    storageBucket: "map-van-times.firebasestorage.app",
-    messagingSenderId: "967630671909",
-    appId: "1:967630671909:web:8985ce0e4f1018f7d7efcc",
-    measurementId: "G-JBS29H6ZZ5"
+  apiKey: "AIzaSyCYMHp-FO1TH8kGIOE42tfNal3sGXV8l6k",
+  authDomain: "map-van-times-1f688.firebaseapp.com",
+  projectId: "map-van-times-1f688",
+  storageBucket: "map-van-times-1f688.firebasestorage.app",
+  messagingSenderId: "169514675608",
+  appId: "1:169514675608:web:e17a3e687efc62d347cfed",
+  measurementId: "G-9SFHTDWRNX"
 };
 
-console.log('Initializing Firebase...');
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+console.log('Initializing Firebase...');
 console.log('Firebase initialized');
 
 console.log('Getting Firestore instance...');
-const db = getFirestore(app);
 console.log('Firestore instance obtained');
 
 module.exports = { db };
